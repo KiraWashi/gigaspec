8f5f38f2c51a69ae020802e053a80d45e9496dc7
NathanaelMo <monnier.nathanael29@gmail.com>
2024-12-16 14:34:17 +0100

    Test

--- src/prettyPrinter/JavaGeneratorTest.java
+++ src/prettyPrinter/JavaGeneratorTest.java
@@ -25,4 +25,13 @@ class JavaGeneratorTest {
 		System.out.println(pp.result());
 	}
 
+	@Test
+	void testPOURNOUS2() {
+		XMLAnalyser analyser = new XMLAnalyser();
+		Model model = analyser.getModelFromFilenamed("ExemplePOURNOUS2.xml");
+		JavaGenerator pp = new JavaGenerator();
+		model.accept(pp);
+		System.out.println(pp.result());
+	}
+
 }
