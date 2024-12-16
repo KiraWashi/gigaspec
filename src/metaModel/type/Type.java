d0b298c267aea8c3f33b00682ead4fa89d41c524
NathanaelMo <monnier.nathanael29@gmail.com>
2024-12-16 15:02:30 +0100

    Type

--- src/metaModel/type/Type.java
+++ src/metaModel/type/Type.java
@@ -1,8 +1,9 @@
 package metaModel.type;
 
+import metaModel.MinispecElement;
 import metaModel.Visitor;
 
-public abstract class Type {
+public abstract class Type implements MinispecElement {
     private final String nom;
     public Type(String nom) {
         this.nom = nom;
